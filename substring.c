334f85b5acd1c415c2a20cda8e72e3de649beae5
stepin104597 <69037767+stepin104597@users.noreply.github.com>
2020-08-11 18:12:51 +0530

    Add files via upload

---
+++ substring.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+
+ int main()
+{
+char string[] = ("my_name_is_edcast_future_skills");
+char delimeter[] = ("_");
+
+char *str = strtok(string,delimeter);
+while(str!= NULL)
+ {
+   printf("\"%s\"\n",str);
+   str = strtok(NULL,delimeter);
+ }
+ printf("\n\n************Enter a sentence with words seperated by '_'**************\n\n");
+char userinput[50];   //enter a sentence spaced by '_' instead of whitespace
+gets(userinput);
+char *p =strtok(userinput,delimeter);
+
+while(p!= NULL)
+ {
+   printf("\"%s\"\n",p);
+   p = strtok(NULL,delimeter);
+ }
+ return (0);
+}
